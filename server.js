e8fa71453139d5aa841fcc122c35c54f55e0a34a
Ricardo Behrens <behrensricardo@gmail.com>
2023-09-09 11:19:50 -0600

    More pseudo code

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(express.static('public'));
 
 // When click on the eventListener it shoul take you to the note.html (I don't think it's another html file but I'm not sure)
 
-// Old notes should be store (is it locally or on the server? It probaby would be on the cloud/server) will appear on the left colunm, while if you would like to take new note, there are empy fields with another eventListener to save the data. Also, the old notes are link that will take you to the notes (not another html, it just appears on the right colunm)
+// Old notes should be store (is it locally or on the server? It probaby would be on the cloud/server) will appear on the left colunm, while if you would like to take new note, there are empy fields with another eventListener to save the data. Also, the old notes are link that will take you to the notes (not another html, it just appears on the right colunm). On the right colunm there is the "note title" and the "note text". On the left colunm you only see the "note title"
+
+// What are the elemento in the body object??? Note title and note text?
 
 // Old notes can be mdified once you have clicked on them and they are on the right colunm
 
