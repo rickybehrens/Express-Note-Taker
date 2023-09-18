2f7e7700c54b44bc872e6d3699bc3ce16680c48f
Ricardo Behrens <behrensricardo@gmail.com>
2023-09-18 11:00:51 -0600

    Basically, I screwed up by changing the front end. I reverted back all the changes and added the 2 routes that I needed. I tried to add the bonus of delete route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.Port || 3001;
 
@@ -12,6 +13,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// GET /notes should return the notes.html file.
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    // console.log(data)
+    var info = JSON.parse(data);
+    // console.log(info)
+    res.json(info)
+  })
+});
+
 app.post('/api/notes', (req, res) => {
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
@@ -22,10 +36,11 @@ app.post('/api/notes', (req, res) => {
     const newNote = {
       title,
       text,
-    };
-
+    };  
+    newNote.id = uuidv4()
+    console.log(newNote)
     // Read existing notes from file
-    fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
       } else {
@@ -37,7 +52,7 @@ app.post('/api/notes', (req, res) => {
 
         // Write updated notes back to the file
         fs.writeFile(
-          './db/notes.json',
+          './db/db.json',
           JSON.stringify(parsedNotes, null, 4),
           (writeErr) => {
             if (writeErr) {
@@ -49,22 +64,50 @@ app.post('/api/notes', (req, res) => {
               const response = {
                 status: 'success',
                 body: newNote,
-              };
+              };  
               res.json(response);
-            }
-          }
-        );
-      }
-    });
+            }  
+          }  
+        );  
+      }  
+    });  
   } else {
     // If title or text is missing, send an error response
     res.status(400).json({ error: 'Both title and text are required.' });
-  }
+  }  
+});  
+
+// DELETE /api/notes/:id to delete a note by ID
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNotes = JSON.parse(data);
+      const updatedNotes = parsedNotes.filter((note) => note.id !== noteId);
+
+      fs.writeFile('./db/db.json', JSON.stringify(updatedNotes, null, 4), (writeErr) => {
+        if (writeErr) {
+          console.error(writeErr);
+        } else {
+          console.info(`Note with ID ${noteId} deleted successfully.`);
+          res.sendStatus(200); // Send a success response
+        }
+      });
+    }
+  });
 });
 
 // Route to handle the /notes path and render the notes.html page
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+//GET * should return the index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(PORT, () =>
